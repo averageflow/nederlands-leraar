c697e65a1c30ac755e7b6f2eb2469091449b7cf5
averageflow <jjbigorra@gmail.com>
2021-12-19 13:10:43 +0100

    add scene change

--- app/src/main/java/nl/averageflow/nederlandsleraar/controllers/StartScreenController.java
+++ app/src/main/java/nl/averageflow/nederlandsleraar/controllers/StartScreenController.java
@@ -1,19 +1,33 @@
 package nl.averageflow.nederlandsleraar.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import nl.averageflow.nederlandsleraar.NederlandsLeraar;
+
+import java.io.IOException;
 
 public class StartScreenController {
+
     @FXML
-    private Label welcomeText;
+    protected VBox startScreenMainContainer;
 
     @FXML
     protected void onClickButtonMetZonderE() {
-        welcomeText.setText("Welkom op Nederlands Leraar");
+       // welcomeText.setText("Welkom op Nederlands Leraar");
     }
 
     @FXML
-    protected void onClickButtonLidwoorden() {
-        welcomeText.setText("Welkom op Nederlands Leraar");
+    protected void onClickButtonLidwoorden() throws IOException {
+        Stage stage = (Stage) startScreenMainContainer.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(NederlandsLeraar.class.getResource("LidwoordenScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setTitle("Lidwoord Spelletje");
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
